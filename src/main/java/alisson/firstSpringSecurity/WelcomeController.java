411c7bddf2b631d2c057d222a02efbb13a5eb149
Alisson Mayer Medeji <alissonmmedeji@gmail.com>
2024-03-27 13:37:38 -0300

    Add WebSecurityConfig, WelcomeController

---
+++ src/main/java/alisson/firstSpringSecurity/WelcomeController.java
@@ -0,0 +1,25 @@
+package alisson.firstSpringSecurity;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcome(){
+        return "Default Welcome";
+    }
+
+    @GetMapping("/users")
+    @PreAuthorize("hasAnyRole('MANAGERS','USERS')")
+    public String users(){
+        return "Authorized user or manager";
+    }
+
+    @GetMapping("/managers")
+    @PreAuthorize("hasRole('MANAGERS')")
+    public String managers(){
+        return "Authorized manager";
+    }
+}
