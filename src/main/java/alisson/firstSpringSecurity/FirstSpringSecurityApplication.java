62bf94c2f578b3ce872a564ea1f68eb6612879b3
Alisson Mayer Medeji <alissonmmedeji@gmail.com>
2024-03-27 11:21:32 -0300

    Initial commit

---
+++ src/main/java/alisson/firstSpringSecurity/FirstSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package alisson.firstSpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringSecurityApplication.class, args);
+	}
+
+}
